72706d8cb310b96bf500cd6667fabae3c2174e75
John LeBoeuf-Little <worldnamer@worldnamer.com>
2019-09-24 12:48:13 -0600

    Add default user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create_with(password: 'password').find_or_create_by(username: 'worldnamer')
